10631421447db2c81109b18b522b858f55d157b7
NPLeeWenKang <s10203100@connect.np.edu.sg>
2022-12-16 02:40:36 +0800

    refactor

--- driver.go
+++ driver.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Driver struct {
 	Driver_Id    int    `json:"driver_id"`
 	First_Name   string `json:"first_name"`
@@ -9,4 +14,59 @@ type Driver struct {
 	Id_No        string `json:"id_no"`
 	Car_No       string `json:"car_no"`
 	Is_Available bool   `json:"is_available"`
+}
+
+func getDriver() ([]Driver, error) {
+	dList := make([]Driver, 0)
+	var rows *sql.Rows
+	var err error
+
+	rows, err = db.Query("SELECT * FROM driver")
+
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var data Driver
+		if err := rows.Scan(&data.Driver_Id, &data.First_Name, &data.Last_Name, &data.Mobile_No, &data.Email, &data.Id_No, &data.Car_No, &data.Is_Available); err != nil {
+			return nil, err
+		}
+		dList = append(dList, data)
+	}
+	return dList, nil
+}
+func getDriverFilterId(id *int) ([]Driver, error) {
+	dList := make([]Driver, 0)
+	var rows *sql.Rows
+	var err error
+
+	rows, err = db.Query("SELECT * FROM driver WHERE driver_id = ?", *id)
+
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var data Driver
+		if err := rows.Scan(&data.Driver_Id, &data.First_Name, &data.Last_Name, &data.Mobile_No, &data.Email, &data.Id_No, &data.Car_No, &data.Is_Available); err != nil {
+			return nil, err
+		}
+		dList = append(dList, data)
+	}
+	return dList, nil
+}
+func insertDriver(d Driver) error {
+	fmt.Println(d)
+	_, err := db.Query("INSERT INTO driver(first_name, last_name, mobile_no, email, id_no, car_no, is_available) VALUES (?, ?, ?, ?, ?, ?, false)", d.First_Name, d.Last_Name, d.Mobile_No, d.Email, d.Id_No, d.Car_No)
+	fmt.Println(err)
+	return err
+}
+
+func updateDriver(id int, d Driver) error {
+	_, err := db.Query("UPDATE driver SET first_name = ?, last_name = ?, mobile_no = ?, email = ?, car_no = ? WHERE driver_id = ?", d.First_Name, d.Last_Name, d.Mobile_No, d.Email, d.Car_No, id)
+	return err
+}
+func updateDriverAvailability(id int, d Driver) error {
+	fmt.Println(id, d.Is_Available)
+	_, err := db.Query("UPDATE driver SET is_available = ? WHERE driver_id = ?", d.Is_Available, id)
+	return err
 }
--- passanger.go
+++ passanger.go
@@ -1,9 +1,59 @@
 package main
 
+import "database/sql"
+
 type Passanger struct {
 	Passanger_Id int    `json:"passanger_id"`
 	First_Name   string `json:"first_name"`
 	Last_Name    string `json:"last_name"`
 	Mobile_No    string `json:"mobile_no"`
 	Email        string `json:"email"`
+}
+
+func getPassanger() ([]Passanger, error) {
+	dList := make([]Passanger, 0)
+	var rows *sql.Rows
+	var err error
+
+	rows, err = db.Query("SELECT * FROM passanger")
+
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var data Passanger
+		if err := rows.Scan(&data.Passanger_Id, &data.First_Name, &data.Last_Name, &data.Mobile_No, &data.Email); err != nil {
+			return nil, err
+		}
+		dList = append(dList, data)
+	}
+	return dList, nil
+}
+func getPassangerFilterId(id *int) ([]Passanger, error) {
+	dList := make([]Passanger, 0)
+	var rows *sql.Rows
+	var err error
+
+	rows, err = db.Query("SELECT * FROM passanger WHERE passanger_id = ?", *id)
+
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var data Passanger
+		if err := rows.Scan(&data.Passanger_Id, &data.First_Name, &data.Last_Name, &data.Mobile_No, &data.Email); err != nil {
+			return nil, err
+		}
+		dList = append(dList, data)
+	}
+	return dList, nil
+}
+func insertPassanger(p Passanger) error {
+	_, err := db.Query("INSERT INTO passanger(passanger_id, first_name, last_name, mobile_no, email) VALUES (?, ?, ?, ?, ?)", p.Passanger_Id, p.First_Name, p.Last_Name, p.Mobile_No, p.Email)
+	return err
+}
+
+func updatePassanger(id int, p Passanger) error {
+	_, err := db.Query("UPDATE passanger SET first_name = ?, last_name = ?, mobile_no = ?, email = ? WHERE passanger_id = ?", p.First_Name, p.Last_Name, p.Mobile_No, p.Email, id)
+	return err
 }
